111b7363d81a6202b84dd070c40e469bfb346066
roguezox <ak9001099436@gmail.com>
2024-08-26 10:52:44 +0530

    origins update

--- main.py
+++ main.py
@@ -117,7 +117,7 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
     access_token = auth.create_access_token(
         data={"sub": user.email}, expires_delta=access_token_expires
     )
-    response = JSONResponse()
+    response = JSONResponse({"access_token": access_token, "token_type": "bearer", "first_name": name})
     response.headers["Access-Control-Allow-Origin"] = "https://backpackkfast-fcvonqkgya-el.a.run.app"
     return response
 
