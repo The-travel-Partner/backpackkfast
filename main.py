733b491f728038d6e684944c52b7e84cc886e082
Aditya Kumar <37246449+roguezox@users.noreply.github.com>
2025-12-24 16:20:16 +0530

    Remove API key from main.py
    Remove API key from main.py for security reasons.

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ from authenticate.authentication import authenticate
 SECRET_KEY = "83daa0256a2289b0fb23693bf1f6034d44396675749244721a2b20e896e11662"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 43800
-apikey= "AIzaSyAt9_35pEEtevoHJCTeJwynPqjx-9-MVjk"
+apikey= ""
 auth = authenticate(secretkey=SECRET_KEY, algorithm=ALGORITHM, usercollection=usercollection)
 origin_url= "https://backpackk-cloud.el.r.appspot.com/"
 
